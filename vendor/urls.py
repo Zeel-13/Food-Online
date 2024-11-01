6cfe17ad9c8ed7c671e2c8577bb4ed2425f2cbe0
Zeel-13 <zeeldhengre265512@gmail.com>
2024-11-01 22:35:51 +0530

    vendor menu category CRUD functionalities

--- vendor/urls.py
+++ vendor/urls.py
@@ -5,4 +5,11 @@ from accounts import views as AccountViews
 urlpatterns = [
     path('profile/',v_profile,name="v_profile"),
     path('',AccountViews.vendorDashboard,name="vendor"),
+    path('menu-builder/',menu_builder,name="menu_builder"),
+    path('menu-builder/category/<int:pk>/',food_items_by_category,name="food_items_by_category"),
+
+    #Category CRUD
+    path('menu-builder/category/add/',add_category,name="add_category"),
+    path('menu-builder/category/edit/<int:pk>/',edit_category,name="edit_category"),
+    path('menu-builder/category/delete/<int:pk>/',delete_category,name="delete_category"),
 ]
